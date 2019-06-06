c4dad5748cbd3e9b6502c31a01194e358023a785
SanekGt <51110741+SanekGt@users.noreply.github.com>
2019-06-06 09:59:47 +0300

    Add files via upload

---
+++ dictionary_123/UnitTestProjectDictionary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("331b825b-926d-497c-908c-7d0a984d7a81")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ dictionary_123/UnitTestProjectDictionary/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dictionary_123;
+using System.Collections.Generic;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+      
+
+        FreqClass freq = new FreqClass();
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Dictionary<string, int> result = freq.FreqCount(@"");
+
+            if (result == null)
+                Assert.Fail();
+
+        }
+
+
+        [TestMethod]
+        public void Extension()
+        {
+
+            Dictionary<string, int> result = freq.FreqCount(@"c:\temp\bmw.jpg");
+
+            if (result == null)
+                Assert.Fail();
+
+        }
+
+
+
+    
+    }
+}
