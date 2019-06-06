90b585fb1dabd3fbe6635e2c75e76ba7dd30a9c0
SanekGt <51110741+SanekGt@users.noreply.github.com>
2019-06-06 09:39:59 +0300

    Add files via upload

---
+++ dictionary_123/FreqClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace dictionary_123
+{
+    public class FreqClass
+    {
+
+
+        public Dictionary<string,int> FreqCount(string path)
+        {
+            Dictionary<string, int> dict = null;
+
+            if(!File.Exists(path) ||(!Path.GetExtension(path).EndsWith("txt")) && !Path.GetExtension(path).EndsWith("doc") 
+            && !Path.GetExtension(path).EndsWith("docx") && !Path.GetExtension(path).EndsWith("rtf"))
+            {
+
+                return dict;
+            }
+
+            dict = new Dictionary<string, int>();
+
+
+            FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream, Encoding.Default);
+
+            string text = reader.ReadToEnd();
+
+            reader.Close();
+            stream.Close();
+
+            MatchCollection matches = Regex.Matches(text, "[A-Z|a-z]+");
+
+            foreach (Match match in matches)
+            {
+                if (!dict.ContainsKey(match.Value))
+                {
+                    dict.Add(match.Value, 1);
+                }
+                else
+                {
+                    dict[match.Value]++;
+                }
+            }
+            return dict;
+        }
+    }
+}
---
+++ dictionary_123/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary_123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FreqClass freq = new FreqClass();
+
+            Dictionary<string, int> result = freq.FreqCount(@"c:\test\file.txt");
+
+
+            FileStream stream = new FileStream(@"c:\test\result.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(stream, Encoding.Default);
+          
+
+            foreach (string word in result.Keys)
+            {
+
+                writer.WriteLine(word + ": " + result[word]);
+
+            }
+
+            writer.Close();
+            stream.Close();
+
+        }
+    }
+}
